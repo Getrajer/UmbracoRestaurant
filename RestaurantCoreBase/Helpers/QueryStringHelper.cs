8869cfde8b2b388e1b001766782e425831aaf0e7
Mateusz Owczarek <getrajer533@gmail.com>
2020-11-29 18:25:29 +0000

    m

---
+++ RestaurantCoreBase/Helpers/QueryStringHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantCoreBase.Helpers
+{
+    public static class QueryStringHelper
+    {
+        public static int GetInt(HttpRequestBase )
+    }
+}
